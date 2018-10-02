7559ad42293cfec6068bca49a708fc1ac03f8737
Ganesh <ganesh@localhost.localdomain>
2018-10-02 23:04:33 +0530

    done

--- api/model/User.js
+++ api/model/User.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose');
 
 var schema=new mongoose.Schema({
-    name:String,
+    name:{
+        type:String,
+    required:true
+}
 });
 
 module.exports=mongoose.model('User',schema);
