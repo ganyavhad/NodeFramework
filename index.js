6562c11acc276aae6c49c32f543cc1cf8ff59655
Ganesh <ganesh@localhost.localdomain>
2018-10-06 23:23:02 +0530

    changes

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.use(express.json());
-var User=require('./api/model/User.js')
+var User=require('./api/model/User.js');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/database',{ useNewUrlParser: true } ).then(()=>console.log('connected to mongodb')).catch(err=>console.error('connection failed'))
+mongoose.connect('mongodb://localhost:27017/database',{ useNewUrlParser: true } ).then(()=>console.log('connected to mongodb')).catch(err=>console.error('connection failed'));
 
 const demoSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true
     }
-})
+});
 const Demo=mongoose.model('demo',demoSchema);
 
 app.get('/',(req,res)=>{
     res.write('Hello we done it with express');
     res.end();
-})
+});
 app.post('/saveData',async(req,res)=>{
       const user=new User(req.body);
       try {
